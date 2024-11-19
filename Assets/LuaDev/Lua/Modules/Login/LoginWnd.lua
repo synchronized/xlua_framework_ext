18749566921b79d0167094a3637d3b0d398b1a0d
synchronized <synchronized@yeah.net>
2024-11-19 20:03:37 +0800

    登录-使用XUUI简化数据绑定

--- Assets/LuaDev/Lua/Modules/Login/LoginWnd.lua
+++ Assets/LuaDev/Lua/Modules/Login/LoginWnd.lua
@@ -10,34 +10,36 @@ end
 function LoginWnd:Awake()
     self.super.Awake(self)
 
-    local txtUsername = self.transform:Find("UIWindow/panContent/panInput/txtAccount"):GetComponent(typeof(TMPro.TMP_InputField))
-    local txtPassword = self.transform:Find("UIWindow/panContent/panInput/txtPassword"):GetComponent(typeof(TMPro.TMP_InputField))
-
     --设置保存的用户名和密码
-    txtUsername.text = PlayerPrefs.GetString("PLAYERINFO.USERNAME")
-    txtPassword.text = PlayerPrefs.GetString("PLAYERINFO.PASSWORD")
-
-    self.onLoginBtnClick = function()
-        local username = txtUsername.text
-        local password = txtPassword.text
-        playerInfo.LoginInfo.username = username
-        playerInfo.LoginInfo.password = password
-
-        --保存用户名和密码
-        PlayerPrefs.SetString("PLAYERINFO.USERNAME", username)
-        PlayerPrefs.SetString("PLAYERINFO.PASSWORD", password)
-        Log(string.format("username: %s, password: %s", username, password))
-        --CommandManager.Execute(CommandID.DoLogin)
-        --TODO 临时直接进入主界面
-        CommandManager.Execute(CommandID.OpenUI, "LobbyMainMgr")
-    end
-
-    self.btnLogin = self.transform:Find("UIWindow/panContent/btnLogin"):GetComponent(typeof(UnityEngine.UI.Button));
-    self.btnLogin.onClick:AddListener(self.onLoginBtnClick)
+    playerInfo.LoginInfo.username = PlayerPrefs.GetString("PLAYERINFO.USERNAME")
+    playerInfo.LoginInfo.password = PlayerPrefs.GetString("PLAYERINFO.PASSWORD")
 end
 
-function LoginWnd:OnDestroy()
-    self.btnLogin.onClick:RemoveListener(self.onLoginBtnClick)
+function LoginWnd:GetViewModel()
+    return {
+        data = {
+            loginInfo = {
+                username = playerInfo.LoginInfo.username,
+                password = playerInfo.LoginInfo.password,
+            },
+        },
+        commands = {
+            onLogin = function (data)
+                local username = data.loginInfo.username
+                local password = data.loginInfo.password
+                playerInfo.LoginInfo.username = username
+                playerInfo.LoginInfo.password = password
+
+                --保存用户名和密码
+                PlayerPrefs.SetString("PLAYERINFO.USERNAME", username)
+                PlayerPrefs.SetString("PLAYERINFO.PASSWORD", password)
+                Log(string.format("username: %s, password: %s", username, password))
+                --CommandManager.Execute(CommandID.DoLogin)
+                --TODO 临时直接进入主界面
+                CommandManager.Execute(CommandID.OpenUI, "LobbyMainMgr")
+            end
+        },
+    }
 end
 
 return LoginWnd
